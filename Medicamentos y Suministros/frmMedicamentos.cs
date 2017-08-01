9f142ddda0a832195ef6e2bcc439e28ac194e31a
michelle-alavrez <alvarezmichelleg@gmail.com>
2017-07-31 18:34:21 -0600

    DataSet Delete

--- Medicamentos y Suministros/frmMedicamentos.cs
+++ Medicamentos y Suministros/frmMedicamentos.cs
@@ -19,16 +19,6 @@ namespace Medicamentos_y_Suministros
 
         private void button5_Click(object sender, EventArgs e)
         {
-            //DateTime Marley = Convert.ToDateTime(dtpvencimiento.Value.ToShortDateString());
-            tableAdapterManager._medicamentosTableAdapter.InsertQuery(txtmedicamento.Text, cantidad.Value, reorden.Value, dtpvencimiento.Text);
-        }
-
-        private void _medicamentosBindingSource1BindingNavigator1SaveItem_Click(object sender, EventArgs e)
-        {
-            this.Validate();
-            this._medicamentosBindingSource1.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.dSMantenimiento);
-
         }
     }
 }
