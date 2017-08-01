ef32dfa3c784d23765ae1b11ccb5d6f02ba19b3b
michelle-alavrez <alvarezmichelleg@gmail.com>
2017-07-31 18:28:53 -0600

    Borrado de DataSet

--- Consulta/frmConsulta.cs
+++ Consulta/frmConsulta.cs
@@ -19,9 +19,7 @@ namespace Consulta
 
         private void frmConsulta_Load(object sender, EventArgs e)
         {
-            // TODO: esta línea de código carga datos en la tabla 'dataSetConsulta.DataTable1' Puede moverla o quitarla según sea necesario.
-            this.dataTable1TableAdapter.Fill(this.dataSetConsulta.DataTable1);
 
-        }
+         }
     }
 }
